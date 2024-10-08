c3e1dbea170832d0c71e497fd688bea8a1f418e7
Firafir Mesele <firafirmessele@gmail.com>
2024-10-08 12:55:43 +0300

    Create a cusom hook to fetch the Games

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -2,25 +2,11 @@ import React, { useEffect, useState } from "react";
 import apiClient from "../Services/api-client";
 import axios from "axios";
 import { Text } from "@chakra-ui/react";
+import useGame from "../Hook/useGame";
 
-interface Game {
-  id: number;
-  name: string;
-}
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+
 const GamesGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const {games ,error}= useGame()
   return (
     <>
       {error && <Text>{error}</Text>}
