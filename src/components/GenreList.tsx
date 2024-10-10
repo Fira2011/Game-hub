74515d4adff4f71aff190682a959c08e6cb0b3d0
Firafir Mesele <firafirmessele@gmail.com>
2024-10-10 15:02:10 +0300

    Displaying Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,25 @@
+import { HStack, Image, List, ListIcon, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../Hook/useGenre";
+import getCropedImageUrl from "../Services/image-url";
+
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
-      {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={'5px'}>
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              src={getCropedImageUrl(genre.image_background)}
+              borderRadius={8}
+            />
+            <Text fontSize={'lg'}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
